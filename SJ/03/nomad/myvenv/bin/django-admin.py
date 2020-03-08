f73ac7dee050d699229f77a6ab138c452c409e5d
jun <bnmva23@hanmail.net>
2020-03-08 22:31:40 +0900

    190308_에어비엔비클론

---
+++ SJ/03/nomad/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hyeonseongjun/Desktop/개발/django pro/nomad/myvnev/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
