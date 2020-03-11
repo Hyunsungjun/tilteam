5e6e78450ab91987183bf8be1f7afcd1f126efb7
jun <bnmva23@hanmail.net>
2020-03-12 00:00:16 +0900

    190312_"room"

--- SJ/03/nomad/rooms/models.py
+++ SJ/03/nomad/rooms/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Room(core_models.TimeStampedModel):
+
+    """ Room Model Definition """
+    
+    name = models.CharField(max_lenght=140)
+    description = models.TextField()
+    country = 
